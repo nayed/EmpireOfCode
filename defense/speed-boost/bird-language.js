8e8ec0686e75797e01f79b188cfff55381c4af0c
Nayed <nayed.saidali@gmail.com>
2015-08-08 01:26:10 +0200

    bird language

---
+++ defense/speed-boost/bird-language.js
@@ -0,0 +1,31 @@
+"use strict"
+
+var VOWELS = "aeiouy"
+
+function translate(phrase) {
+    i = 0
+    trad = ""
+    while (i < phrase.length){
+        if (phrase[i] == ' '){
+            trad += phrase[i]
+            i++
+        } else if (/[aeiouy/]+/.test(phrase.charAt(i))){
+            trad += phrase.charAt(i)
+            i += 3
+        } else {
+            trad += phrase.charAt(i)
+            i += 2
+        }
+    }
+    return trad
+}
+
+var assert = require("assert")
+
+if (!global.is_checking) {
+    // These "asserts" using only for self-checking and not necessary for auto-testing
+    assert.equal(translate("hieeelalaooo"), "hello", "Hi!")
+    assert.equal(translate("hoooowe yyyooouuu duoooiiine"), "how you doin", "Joey?")
+    assert.equal(translate("aaa bo cy da eee fe"), "a b c d e f", "Alphabet")
+    assert.equal(translate("sooooso aaaaaaaaa"), "sos aaa", "Mayday, mayday")
+}
