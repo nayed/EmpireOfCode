312c4d4ed2b8732e0c1bd96fcace57c85e306495
Nayed <nayed.saidali@gmail.com>
2015-08-18 22:45:26 +0200

    count ingots: functional programming way

--- resource/count-ingots/count-ingots.js
+++ resource/count-ingots/count-ingots.js
@@ -4,22 +4,23 @@ function countIngots(report) {
     var number = null
     var result = null
     var arrayReport = report.split(',')
-    var ALPHABET = {
-        0: "A", 1: "B", 2: "C", 3: "D", 4: "E", 5: "F",
-        6: "G", 7: "H", 8: "I", 9: "J", 10: "K", 11: "L",
-        12: "M", 13: "N", 14: "O", 15: "P", 16: "Q", 17: "R",
-        18: "S", 19: "T", 20: "U", 21: "V", 22: "W", 23: "X",
-        24: "Y", 25: "Z"
-    }
+    var ALPHABET = [
+        "A", "B", "C", "D", "E", "F",
+        "G", "H", "I", "J", "K", "L",
+        "M", "N", "O", "P", "Q", "R",
+        "S", "T", "U", "V", "W", "X",
+        "Y", "Z"
+    ]
     
-    for (var key in ALPHABET) {
-        for (var i = 0; i < arrayReport.length; i++) {
-            if (arrayReport[i].charAt(0) === ALPHABET[key]) {
-                number = parseInt(arrayReport[i].charAt(1))
-                result += key * 10 + number - key
+    ALPHABET.map(function(letter) {
+        arrayReport.map(function(rep) {
+            if (rep.charAt(0) === letter) {
+                number = parseInt(rep.charAt(1))
+                var indexLetter = ALPHABET.indexOf(letter)
+                result += indexLetter * 10 + number - indexLetter
             }
-        }
-    }
+        })
+    })
     return result
 }
 
